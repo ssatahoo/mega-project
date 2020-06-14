40f4815c86deebcd056ea67d9e548e1ec13c6c06
Sachin Satahoo <ssatahoo@gmail.com>
2020-06-14 00:00:00 -0400

    feat: add missing file checker

--- repo-linter/linter.py
+++ repo-linter/linter.py
@@ -86,3 +86,4 @@ if __name__ == "__main__":
 # commit 30 - chore: clean print statements
 # commit 31 - refactor: break out functions
 # commit 32 - feat: add forbidden file check
+# commit 34 - feat: add missing file checker
