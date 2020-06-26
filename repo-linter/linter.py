70354a2110b4f1aa8cdee2872d40381dbd06bb52
Sachin Satahoo <ssatahoo@gmail.com>
2020-06-26 00:00:00 -0400

    refactor: break out functions

--- repo-linter/linter.py
+++ repo-linter/linter.py
@@ -93,3 +93,4 @@ if __name__ == "__main__":
 # commit 38 - feat: add long path detection
 # commit 39 - refactor: break out functions
 # commit 40 - refactor: break out functions
+# commit 41 - refactor: break out functions
