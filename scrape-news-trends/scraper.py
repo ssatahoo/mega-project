d3c4b4afda51d23cdbed3d223859bcce1e145a18
Sachin Satahoo <ssatahoo@gmail.com>
2020-09-05 00:00:00 -0400

    chore: move scraped data to subfolder

--- scrape-news-trends/scraper.py
+++ scrape-news-trends/scraper.py
@@ -34,3 +34,4 @@ if __name__ == "__main__":
 # update 37 - fix: error on empty headline list
 # update 40 - refactor: separate save function
 # update 41 - feat: add matplotlib plot
+# update 47 - chore: move scraped data to subfolder
