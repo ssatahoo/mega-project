426c06e7399667706663be55e24a6b8ba8f33a4d
Sachin Satahoo <ssatahoo@gmail.com>
2020-09-14 00:00:00 -0400

    feat: analyze word frequency

--- scrape-news-trends/scraper.py
+++ scrape-news-trends/scraper.py
@@ -38,3 +38,4 @@ if __name__ == "__main__":
 # update 48 - docs: explain JSON format
 # update 50 - feat: analyze word frequency
 # update 51 - feat: add TechCrunch scrape support
+# update 52 - feat: analyze word frequency
